e48b27fb7c8306254ae42472969c2ed0704d49f0
Evgeny Kuprikov <evggamgam@gmail.com>
2021-10-27 17:32:49 +0700

    commit

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 
-x = np.linspace(0, 2 * np.pi, 10000000)
+x = np.linspace(0, 2 * np.pi, 100)
 y = np.sin(x)
 y2 = np.cos(x)
 
-plt.plot(x, y)
-plt.plot(x, y2)
+plt.scatter(x, y, c = y2)
 plt.show()

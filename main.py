e2f7602822a8bb9dfd26632a666c55097e4da755
Evgeny Kuprikov <evggamgam@gmail.com>
2021-10-27 17:29:49 +0700

    commit

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 
-<<<<<<< HEAD
+
 x = np.linspace(0, 2 * np.pi, 1000)
-=======
-x = np.linspace(0, 2 * np.pi, 10000000)
->>>>>>> test
 y = np.sin(x)
 y2 = np.cos(x)
 
